019dc243a0d180d8651b2ca03b75962d5afbc110
prama036 <115654480+prama036@users.noreply.github.com>
2022-10-16 03:30:00 +0700

    perc2

---
+++ other/web.js
@@ -0,0 +1,130 @@
+function carac1() {
+    var x1 = document.getElementById("infcarac1");
+    if (x1.style.display === "block") {
+      x1.style.display = "none";
+    } else {
+      x1.style.display = "block";
+    }
+    var x2 = document.getElementById("infcarac2");
+    if (x2.style.display === "block") {
+        x2.style.display = "none";
+    }
+    var x3 = document.getElementById("infcarac3");
+    if (x3.style.display === "block") {
+        x3.style.display = "none";
+    }
+    var x4 = document.getElementById("infcarac4");
+    if (x4.style.display === "block") {
+        x4.style.display = "none";
+    }
+    var x5 = document.getElementById("infcarac5");
+    if (x5.style.display === "block") {
+        x5.style.display = "none";
+    }
+  }
+
+
+
+  
+function carac2() {
+  var x1 = document.getElementById("infcarac1");
+  if (x1.style.display === "block") {
+    x1.style.display = "none";
+  }
+  var x2 = document.getElementById("infcarac2");
+  if (x2.style.display === "block") {
+      x2.style.display = "none";
+  } else {
+    x2.style.display = "block";
+  }
+  var x3 = document.getElementById("infcarac3");
+  if (x3.style.display === "block") {
+      x3.style.display = "none";
+  }
+  var x4 = document.getElementById("infcarac4");
+  if (x4.style.display === "block") {
+      x4.style.display = "none";
+  }
+  var x5 = document.getElementById("infcarac5");
+  if (x5.style.display === "block") {
+      x5.style.display = "none";
+  }
+    
+  }
+
+
+
+
+
+function carac3() {
+  var x1 = document.getElementById("infcarac1");
+  if (x1.style.display === "block") {
+    x1.style.display = "none";
+  }
+  var x2 = document.getElementById("infcarac2");
+  if (x2.style.display === "block") {
+      x2.style.display = "none";
+  }
+  var x3 = document.getElementById("infcarac3");
+  if (x3.style.display === "block") {
+      x3.style.display = "none";
+  } else {
+    x3.style.display = "block";
+  }
+  var x4 = document.getElementById("infcarac4");
+  if (x4.style.display === "block") {
+      x4.style.display = "none";
+  }
+  var x5 = document.getElementById("infcarac5");
+  if (x5.style.display === "block") {
+      x5.style.display = "none";
+  }
+}
+function carac4() {
+  var x1 = document.getElementById("infcarac1");
+  if (x1.style.display === "block") {
+    x1.style.display = "none";
+  }
+  var x2 = document.getElementById("infcarac2");
+  if (x2.style.display === "block") {
+      x2.style.display = "none";
+  }
+  var x3 = document.getElementById("infcarac3");
+  if (x3.style.display === "block") {
+      x3.style.display = "none";
+  }
+  var x4 = document.getElementById("infcarac4");
+  if (x4.style.display === "block") {
+      x4.style.display = "none";
+  } else {
+    x4.style.display = "block";
+  }
+  var x5 = document.getElementById("infcarac5");
+  if (x5.style.display === "block") {
+      x5.style.display = "none";
+  }
+}
+function carac5() {
+  var x1 = document.getElementById("infcarac1");
+  if (x1.style.display === "block") {
+    x1.style.display = "none";
+  }
+  var x2 = document.getElementById("infcarac2");
+  if (x2.style.display === "block") {
+      x2.style.display = "none";
+  }
+  var x3 = document.getElementById("infcarac3");
+  if (x3.style.display === "block") {
+      x3.style.display = "none";
+  }
+  var x4 = document.getElementById("infcarac4");
+  if (x4.style.display === "block") {
+      x4.style.display = "none";
+  }
+  var x5 = document.getElementById("infcarac5");
+  if (x5.style.display === "block") {
+      x5.style.display = "none";
+  } else {
+    x5.style.display = "block";
+  }
+  }
